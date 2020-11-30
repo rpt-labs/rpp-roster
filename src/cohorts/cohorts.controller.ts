3ed736ac1c812d0f526fb028508ad96fd43161f0
debalina <mukherjee.debalina@gmail.com>
2020-11-29 23:48:34 -0800

    add support update and delete cohort

--- src/cohorts/cohorts.controller.ts
+++ src/cohorts/cohorts.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Post, Put, Delete, Body, Param  } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { Cohort } from './cohort.entity';
 import { CohortsService } from './cohorts.service';
 
 @Controller('cohorts')
 export class CohortsController {
-  constructor(private cohortsService: CohortsService) {}
+  constructor(private cohortsService: CohortsService) { }
 
   @Get()
   index(): Promise<Cohort[]> {
@@ -14,5 +14,17 @@ export class CohortsController {
   @Post('create')
   async create(@Body() cohortData: Cohort): Promise<any> {
     return this.cohortsService.create(cohortData);
+  }
+
+  @Put(':id/update')
+  async update(@Param('id') id, @Body() cohortData: Cohort): Promise<any> {
+    cohortData.id = Number(id);
+    console.log('Update #' + cohortData.id)
+    return this.cohortsService.update(cohortData);
+  }
+
+  @Delete(':id/delete')
+  async delete(@Param('id') id): Promise<any> {
+    return this.cohortsService.delete(id);
   }
 }
