6e94702bfee9e596f331597c1b0b4241af03d282
debalina <mukherjee.debalina@gmail.com>
2020-11-29 16:55:33 -0800

    add cohort details

--- src/cohorts/cohorts.controller.ts
+++ src/cohorts/cohorts.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param  } from '@nestjs/common';
 import { Cohort } from './cohort.entity';
 import { CohortsService } from './cohorts.service';
 
 @Controller('cohorts')
 export class CohortsController {
-  constructor(private contactsService: CohortsService) {}
+  constructor(private cohortsService: CohortsService) {}
 
   @Get()
   index(): Promise<Cohort[]> {
-    return this.contactsService.findAll();
+    return this.cohortsService.findAll();
+  }
+
+  @Post('create')
+  async create(@Body() cohortData: Cohort): Promise<any> {
+    return this.cohortsService.create(cohortData);
   }
 }
