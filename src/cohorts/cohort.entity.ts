6e5d62791dcfc08e54d50871c5a07569b3face39
debalina <mukherjee.debalina@gmail.com>
2020-12-21 06:24:27 -0800

    feat(cohorts): update cohort entity

--- src/cohorts/cohort.entity.ts
+++ src/cohorts/cohort.entity.ts
@@ -6,14 +6,74 @@ export class Cohort {
   id: number;
 
   @Column()
-  cohortName: string;
+  cohort_id: string;
 
   @Column()
-  cohortId: number;
+  learn_id: number;
 
   @Column()
-  startDate: string;
+  product_code: string;
 
   @Column()
-  endDate: string;
+  rotation_id: string;
+
+  @Column()
+  schedule: string;
+
+  @Column()
+  start_date: Date;
+
+  @Column()
+  grad_date: Date;
+
+  @Column()
+  fifty_percent: Date;
+
+  @Column()
+  sixty_percent: Date;
+
+  @Column()
+  google_calendar_id: string;
+
+  @Column()
+  cohort_lead: string;
+
+  @Column()
+  tech_mentor: string;
+
+  @Column()
+  senior_phase_begins: Date;
+
+  @Column()
+  fec_start: Date;
+
+  @Column()
+  sdc_start: Date;
+
+  @Column()
+  outcome_start: Date;
+
+  @Column()
+  solo_1_start: Date;
+
+  @Column()
+  solo_1_end: Date;
+
+  @Column()
+  solo_2_start: Date;
+
+  @Column()
+  solo_2_end: Date;
+
+  @Column()
+  holiday_1_start: Date;
+
+  @Column()
+  holiday_1_end: Date;
+
+  @Column()
+  holiday_2_start: Date;
+
+  @Column()
+  holiday_2_end: Date;
 }
